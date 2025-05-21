45b7efec85d2bb82bcf7b538b6165ccba6fda104
Jeden1102 <bum123@o2.pl>
2025-05-21 22:04:21 +0200

    master. Primevue + tailwind setup.

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    Testing
+    <div class="flex flex-col gap-4 p-20">
+      <Button label="Verify" class="w-fit" />
+      <Checkbox v-model="checked" binary />
+      <DatePicker v-model="date" class="w-fit" />
+    </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+const checked = ref(true);
+
+const date = ref(null);
+</script>
